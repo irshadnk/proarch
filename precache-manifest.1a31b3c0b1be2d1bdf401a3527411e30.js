e2ae7cfe6631fc7f8fa82013051517b9cab3a7cc
Irshad <irshadnu@gmail.com>
2019-02-25 09:13:50 +0530

    Updates

---
+++ precache-manifest.1a31b3c0b1be2d1bdf401a3527411e30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/proarch-depoy/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/proarch-depoy/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/proarch-depoy/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/proarch-depoy/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "3d245620626a0ac7e53d",
+    "url": "/proarch-depoy/static/js/runtime~main.3d245620.js"
+  },
+  {
+    "revision": "58e3f780eed3c76f1b05",
+    "url": "/proarch-depoy/static/js/main.58e3f780.chunk.js"
+  },
+  {
+    "revision": "22c8d16e8b3e79b7a778",
+    "url": "/proarch-depoy/static/js/1.22c8d16e.chunk.js"
+  },
+  {
+    "revision": "58e3f780eed3c76f1b05",
+    "url": "/proarch-depoy/static/css/main.d45c7fab.chunk.css"
+  },
+  {
+    "revision": "22c8d16e8b3e79b7a778",
+    "url": "/proarch-depoy/static/css/1.ee16840c.chunk.css"
+  },
+  {
+    "revision": "4a43f571525ccd19bda319691a59684e",
+    "url": "/proarch-depoy/index.html"
+  }
+];
